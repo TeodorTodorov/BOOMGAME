5d309aa86e2f70d5f116017ca05de9e53f543156
TeodorTodorov <teodorvulchev@gmail.com>
2014-07-26 15:24:33 +0300

    Finished

---
+++ BombGame/StartBombGame.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BombGame
+{
+    class StartBombGame
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WindowHeight = 16;
+            Console.WindowWidth = 45;
+            
+            Console.SetBufferSize(45,16);
+
+            //
+            bool play = true;
+            GameCore game = new GameCore();
+            while (play == true)
+            {
+                game.Draw();
+                Console.WriteLine();
+                Console.WriteLine("to restart the game type \"restart\"");
+                Console.WriteLine("give me something like /A1/: ");
+
+                string reader = Console.ReadLine();
+
+
+
+                reader = reader.Trim();
+               
+
+                if (reader == "restart")
+                    {
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.WriteLine("REBOOTED");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        game.LoadGrounds();
+
+                    }
+                    else
+                    {
+                        try
+                        {
+                            game.ValidateInput(reader);
+                        }
+                        catch (Exception fee)
+                        {
+                            if (fee.Message == "Invalid input, try again")
+                            {
+                                PrintInvalidInput();
+                                continue;
+
+                            }
+                            else
+                            {
+                                throw fee;
+                            }
+                        }
+                        try
+                        {
+                            game.Checker(reader);
+                        }
+                  
+                        catch (Exception fe)
+                        {
+                            if (fe.Message == "Invalid input, try again")
+                            {
+                                PrintInvalidInput();
+                            }
+                            else
+                            {
+                                throw fe;
+                            }
+
+                        }
+
+                    }
+                }
+
+
+            }
+
+        private static void PrintInvalidInput()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Invalid input try again");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        }
+    }
+
